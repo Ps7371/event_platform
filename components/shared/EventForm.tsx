3f37b4814826e3fe8528955a14eca500074c7520
Ps7371 <prajjwalsingh592@gmail.com>
2025-03-09 13:47:55 +0530

    Updated code with new changes

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -59,15 +59,15 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
   async function onSubmit(values: z.infer<typeof eventFormSchema>) {
     let uploadedImageUrl = values.imageUrl;
 
-    if (files.length > 0) {
-      const uploadedImages = await startUpload(files)
+    // if (files.length > 0) {
+    //   const uploadedImages = await startUpload(files)
 
-      if (!uploadedImages) {
-        return
-      }
+    //   if (!uploadedImages) {
+    //     return
+    //   }
 
-      uploadedImageUrl = uploadedImages[0].url
-    }
+    //   uploadedImageUrl = uploadedImages[0].url
+    // }
 
     if (type === 'Create') {
       try {
